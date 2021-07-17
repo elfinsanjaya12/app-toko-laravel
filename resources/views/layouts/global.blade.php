823923de9d2b832f95af498d63c947ce1e7dd109
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-07-17 21:19:51 +0700

    feat: [Manage Book]

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -77,6 +77,7 @@
                     </a>
               </li>
               <li><a href="{{route('categories.index')}}"><span class="oi oi-tag"></span> Manage Categories</a></li>
+              <li><a href="{{route('books.index')}}"><span class="oi oi-book"></span> Manage Books</a></li>
 
               <div class="d-block d-md-none">
                   <div class="dropdown-divider"></div>
@@ -117,5 +118,6 @@
   crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  @yield('footer-scripts')
 </body>
 </html>
